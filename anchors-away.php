28355b435d10f41c696d2678a1fd26d9c415ba3b
Thomas Leen <tom@confluentforms.com>
2014-09-17 01:03:06 +0000

    added message set option in admin menu

--- anchors-away.php
+++ anchors-away.php
@@ -11,19 +11,65 @@
 defined('ABSPATH') or die;
 
 
+//* -- Client Execution -- *//
 // If url indicates admin page, skip this entirely
 if(!preg_match("/(.*)\/wp-admin\/(.*)/", add_query_arg(array()))){
   wp_enqueue_script('jquery');
   wp_enqueue_script('anchors-away', plugins_url( 'anchors-away.js', __FILE__ ), array('jquery'));
 
   // xx - setup admin screen to get custom message
-  function aa_instantiate(){
+  function aa_embed_footer(){
+    $message = get_option('aa-message-text');
 ?>
 <script type="text/javascript" language="javascript">
-  this.anchorsAway('');
+/* <![CDATA[ */
+  this.anchorsAway(<?php echo json_encode($message) ?>);
+/* ]]> */
 </script>
 <?php
   }
   
-  add_action('wp_footer', 'aa_instantiate'); 
+  add_action('wp_footer', 'aa_embed_footer'); 
+}
+
+//* -- Admin Options Execution -- *//
+
+add_action('admin_menu', 'aa_plugin_menu' );
+
+function aa_plugin_menu() {
+  add_menu_page('Anchors Away Plugin Settings', 'Anchors Away', 'administrator', __FILE__, 'aa_settings_page', 'dashicons-editor-unlink');
+
+  add_action('admin_init', 'aa_register_settings');
+}
+
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'aa_add_action_links');
+
+function aa_add_action_links($links){
+  $links[] = '<a href="' . admin_url('?page=' . plugin_basename(__FILE__)) . '">Settings</a>';
+  return $links;
+}
+
+function aa_register_settings(){
+  // xx - sanitize
+  register_setting('aa-settings-group', 'aa-message-text');
+}
+
+function aa_settings_page(){
+?>
+<div class="wrap">
+  <h2>Anchors Away Settings</h2>
+  <form method="post" action="options.php">
+    <?php settings_fields( 'aa-settings-group' ); ?>
+    <?php do_settings_sections( 'aa-settings-group' ); ?>
+    <table class="form-table">
+      <tr valign="top">
+      <th scope="row" style="width: 80px">Message:</th>
+        <td><input type="text" name="aa-message-text" size="50" value="<?php echo esc_attr(get_option('aa-message-text')) ?>" /></td>
+      </tr>      
+    </table>
+    <p>Set message to display when user clicks on a deactivated link. If message is empty, no message will be shown.</p>
+    <?php submit_button(); ?>
+  </form>
+</div>
+<?php  
 }
